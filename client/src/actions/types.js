47d2d680a2fd24ad9b24269082521e3a161c289a
“[Nicholas <n.samaras4@outlook.com>
2023-03-26 17:31:31 -0400

    update

---
+++ client/src/actions/types.js
@@ -0,0 +1,26 @@
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+
+export const GET_CONTACT = "GET_CONTACT";
+export const GET_CONTACTS = "GET_CONTACTS";
+export const CLEAR_CONTACTS = "CLEAR_CONTACTS";
+export const UPDATE_CONTACT = "UPDATE_CONTACT";
+export const CONTACT_ERROR = "CONTACT_ERROR";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
+export const CLEAR_APPLICATION = "CLEAR_APPLICATION";
+
+export const GET_PROFILE = "GET_PROFILE";
+export const GET_PROFILES = "GET_PROFILES";
+export const GET_REPOS = "GET_REPOS";
+export const PROFILE_ERROR = "PROFILE_ERROR";
+
+export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
