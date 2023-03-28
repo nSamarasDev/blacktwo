fe2dce3db3ed0fbb646831eda8c6fcec1a25c7f1
“[Nicholas <n.samaras4@outlook.com>
2023-03-28 13:47:21 -0400

    iupdate

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Spinner from "../../components/layout/Spinner";
-import { getCurrentProfile } from "../../actions/profile";
+import DashboardActions from "../dashboard/DashboardActions";
+import { getCurrentProfile, deleteAccount } from "../../actions/profile";
 import Alert from "../layout/Alert";
 
-const Dashboard = (props) => {
-  return (
+const Dashboard = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
     <>
       <section className="container">
-        <div className></div>
+        <Alert />
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Welcome {user && user.name}
+        </p>
+        {profile !== null ? (
+          <Fragment>
+            <DashboardActions />
+
+            <div className="my-2">
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteAccount()}
+              >
+                <i className="fas fa-user-minus"></i>Delete my account
+              </button>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </Fragment>
+        )}
       </section>
     </>
   );
@@ -20,6 +57,14 @@ Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
 };
 
-export default connect(mapstaetToProps, { getCurrentProfile })(Dashboard);
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
