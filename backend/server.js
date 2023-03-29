f8e107d7830ef221f4bb96a00c439f1d45f796a4
“[Nicholas <n.samaras4@outlook.com>
2023-03-29 11:12:56 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
-app.use("/api/user", require("./routes/api/user"));
+app.use("/api/users", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
