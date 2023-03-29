ed32be1b2e80d6e17f959cb3fd9a231d9f8782c3
“[Nicholas <n.samaras4@outlook.com>
2023-03-29 11:13:42 -0400

    update

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,6 +20,7 @@ export const CLEAR_APPLICATION = "CLEAR_APPLICATION";
 
 export const GET_PROFILE = "GET_PROFILE";
 export const GET_PROFILES = "GET_PROFILES";
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const GET_REPOS = "GET_REPOS";
 export const PROFILE_ERROR = "PROFILE_ERROR";
 
